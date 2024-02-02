0c648add4d4f64c30c713bc1ab282cf2fd147996
timka <timkaa555@gmail.com>
2024-02-02 16:16:02 +0300

    add personal

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import asyncio
 from config import bot, dp
 import logging
 from handlers.user.main import router
+from handlers.admin.main import router as admin_router
 
 
 async def main():
@@ -11,6 +12,8 @@ async def main():
     )
     await bot.delete_webhook(drop_pending_updates=True)
     dp.include_router(router)
+    dp.include_router(admin_router)
+
     await dp.start_polling(bot)
 
 
