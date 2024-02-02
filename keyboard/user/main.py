ed0bb9a40cae50232e71b8b94266a62001053543
timka <timkaa555@gmail.com>
2024-02-02 16:46:59 +0300

    fix

--- keyboard/user/main.py
+++ keyboard/user/main.py
@@ -1,4 +1,8 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from config import db_session
+from aiogram.filters.callback_data import CallbackData
+from data.models import Object
 
 
 def main():
@@ -12,3 +16,18 @@ def main():
     ]
     markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
     return markup
+
+
+class CallbackObjectData(CallbackData, prefix="object_data"):
+    action: str
+
+
+def performance_report_markup():
+    builder = InlineKeyboardBuilder()
+    data = db_session.query(Object.id, Object.name).all()
+    for d in data:
+        builder.button(
+            text=d[1], callback_data=CallbackObjectData(action=str(d[0]))
+        )
+    return builder.as_markup()
+
